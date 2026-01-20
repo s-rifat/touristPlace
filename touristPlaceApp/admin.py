feb24d07b59e11ec681502a2d35dd958bc9463fb
ENOSISBD\manjur <syed.rifat.manjur@gmail.com>
2026-01-20 14:49:48 +0600

    remove new line

--- touristPlaceApp/admin.py
+++ touristPlaceApp/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Place, PlaceImage, Country
 
-
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
